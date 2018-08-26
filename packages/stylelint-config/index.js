d0c2545b8057dbd59e21b01fc482df50276de04b
Derek P Sifford <dereksifford@gmail.com>
2018-08-26 12:16:30 -0400

    initial commit

---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    extends: ['stylelint-config-recommended-scss'],
+    rules: {
+        'max-empty-lines': [1, { ignore: ['comments'] }],
+        'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
+        'rule-empty-line-before': [
+            'always',
+            { except: ['after-single-line-comment', 'first-nested'] },
+        ],
+        'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['value'] }],
+        'selector-pseudo-class-no-unknown': [
+            true,
+            { ignorePseudoClasses: ['global'] },
+        ],
+    },
+};
