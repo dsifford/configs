3581e04bb63f85e4f9ff3b69d7c09708e163beed
Derek Sifford <dereksifford@gmail.com>
2019-08-20 18:44:55 -0400

    update eslint-config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,7 +1,9 @@
 module.exports = {
     extends: [
         'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:react/recommended',
         'plugin:jest/recommended',
         'plugin:jsx-a11y/recommended',
